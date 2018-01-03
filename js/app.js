361211e0867cb85973566ace95ad47e23f05e821
simplon <dekorte.tsrit@gmail.com>
2018-01-03 16:44:38 +0100

    slight adjustments

--- js/app.js
+++ js/app.js
@@ -1,149 +1,99 @@
-/*
-
-########   source  :   http://jsfiddle.net/ezmilhouse/V2S9d/   #######
+/*   ########   source  :   http://jsfiddle.net/ezmilhouse/V2S9d/   #######
 
 JQUERY: STOPWATCH & COUNTDOWN
-
 This is a basic stopwatch & countdown plugin to run with jquery. Start timer, pause it, stop it or reset it. Same behaviour with the countdown besides you need to input the countdown value in seconds first. At the end of the countdown a callback function is invoked.
-
 Any questions, suggestions? marc.fuehnen(at)gmail.com
 
 */
 
 $(document).ready(function() {
-
     (function($){
-    
         $.extend({
-            
-            APP : {                
-                
+            APP : {    
                 formatTimer : function(a) {
                     if (a < 10) {
                         a = '0' + a;
                     }                              
                     return a;
-                },    
-                
+                },
                 startTimer : function(dir) {
-                    
                     var a;
-                    
                     // save type
                     $.APP.dir = dir;
-                    
                     // get current date
                     $.APP.d1 = new Date();
-                    
                     switch($.APP.state) {
-                            
                         case 'pause' :
-                            
                             // resume timer
                             // get current timestamp (for calculations) and
                             // substract time difference between pause and now
-                            $.APP.t1 = $.APP.d1.getTime() - $.APP.td;                            
-                            
+                            $.APP.t1 = $.APP.d1.getTime() - $.APP.td;
                         break;
-                            
                         default :
-                            
                             // get current timestamp (for calculations)
                             $.APP.t1 = $.APP.d1.getTime(); 
-                            
                             // if countdown add ms based on seconds in textfield
                             if ($.APP.dir === 'cd') {
                                 $.APP.t1 += parseInt($('#cd_seconds').val())*1000;
-                            }    
-                        
+                            } 
                         break;
-                            
-                    }                                   
-                    
+                    } 
                     // reset state
                     $.APP.state = 'alive';   
                     $('#' + $.APP.dir + '_status').html('Running');
-                    
                     // start loop
                     $.APP.loopTimer();
-                    
                 },
-                
                 pauseTimer : function() {
-                    
                     // save timestamp of pause
                     $.APP.dp = new Date();
                     $.APP.tp = $.APP.dp.getTime();
-                    
                     // save elapsed time (until pause)
                     $.APP.td = $.APP.tp - $.APP.t1;
-                    
                     // change button value
                     $('#' + $.APP.dir + '_start').val('Resume');
-                    
                     // set state
                     $.APP.state = 'pause';
                     $('#' + $.APP.dir + '_status').html('Paused');
-                    
                 },
-                
                 stopTimer : function() {
-                    
                     // change button value
-                    $('#' + $.APP.dir + '_start').val('Restart');                    
-                    
+                    $('#' + $.APP.dir + '_start').val('Restart');               
                     // set state
                     $.APP.state = 'stop';
                     $('#' + $.APP.dir + '_status').html('Stopped');
-                    
                 },
-                
                 resetTimer : function() {
-
                     // reset display
-                    $('#' + $.APP.dir + '_ms,#' + $.APP.dir + '_s,#' + $.APP.dir + '_m,#' + $.APP.dir + '_h').html('00');                 
-                    
+                    $('#' + $.APP.dir + '_ms,#' + $.APP.dir + '_s,#' + $.APP.dir + '_m,#' + $.APP.dir + '_h').html('00');
                     // change button value
-                    $('#' + $.APP.dir + '_start').val('Start');                    
-                    
+                    $('#' + $.APP.dir + '_start').val('Start');
                     // set state
                     $.APP.state = 'reset';  
                     $('#' + $.APP.dir + '_status').html('Reset & Idle again');
-                    
                 },
-                
                 endTimer : function(callback) {
-                   
                     // change button value
                     $('#' + $.APP.dir + '_start').val('Restart');
-                    
                     // set state
                     $.APP.state = 'end';
-                    
                     // invoke callback
                     if (typeof callback === 'function') {
                         callback();
-                    }    
-                    
-                },    
-                
+                    } 
+                }, 
                 loopTimer : function() {
-                    
                     var td;
                     var d2,t2;
-                    
                     var ms = 0;
                     var s  = 0;
                     var m  = 0;
                     var h  = 0;
-                    
                     if ($.APP.state === 'alive') {
-                                
                         // get current date and convert it into 
                         // timestamp for calculations
                         d2 = new Date();
-                        t2 = d2.getTime();   
-                        
+                        t2 = d2.getTime();
                         // calculate time difference between
                         // initial and current timestamp
                         if ($.APP.dir === 'sw') {
@@ -158,8 +108,7 @@ $(document).ready(function() {
                                     $('#' + $.APP.dir + '_status').html('Ended & Reset');
                                 });
                             }    
-                        }    
-                        
+                        } 
                         // calculate milliseconds
                         ms = td%1000;
                         if (ms < 1) {
@@ -183,55 +132,39 @@ $(document).ready(function() {
                                 }    
                             }
                         }
-                      
                         // substract elapsed minutes & hours
                         ms = Math.round(ms/100);
                         s  = s-(m*60);
-                        m  = m-(h*60);                                
-                        
+                        m  = m-(h*60);
                         // update display
                         $('#' + $.APP.dir + '_ms').html($.APP.formatTimer(ms));
                         $('#' + $.APP.dir + '_s').html($.APP.formatTimer(s));
                         $('#' + $.APP.dir + '_m').html($.APP.formatTimer(m));
                         $('#' + $.APP.dir + '_h').html($.APP.formatTimer(h));
-                        
                         // loop
                         $.APP.t = setTimeout($.APP.loopTimer,1);
-                    
                     } else {
-                    
                         // kill loop
                         clearTimeout($.APP.t);
                         return true;
-                    
                     }  
-                    
-                }
-                    
-            }    
-        
+                }  
+            } 
         });
-          
         $('#sw_start').live('click', function() {
             $.APP.startTimer('sw');
-        });    
-
+        }); 
         $('#cd_start').live('click', function() {
             $.APP.startTimer('cd');
-        });           
-        
+        });
         $('#sw_stop,#cd_stop').live('click', function() {
             $.APP.stopTimer();
         });
-        
         $('#sw_reset,#cd_reset').live('click', function() {
             $.APP.resetTimer();
-        });  
-        
+        });
         $('#sw_pause,#cd_pause').live('click', function() {
             $.APP.pauseTimer();
-        });                
-                
+        });      
     })(jQuery);
-        
 });
